70780123fe1e355eb010fd9e2b06c1726803726f
Rihitonnnu <hexiliang04@gmail.com>
2022-12-27 22:16:28 +0900

    Reservationモデル作成

---
+++ app/Models/Reservation.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Reservation extends Model
+{
+    use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function facility(): BelongsTo
+    {
+        return $this->belongsTo(Facility::class);
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'facility_id',
+        'starttime',
+        'endtime',
+        'perpose',
+    ];
+}

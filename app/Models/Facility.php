018e0518a38b6319219a31597595b200e5b17fbb
Rihitonnnu <hexiliang04@gmail.com>
2022-12-27 22:27:21 +0900

    FacilityのModel作成

---
+++ app/Models/Facility.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Facility extends Model
+{
+    use HasFactory;
+
+    public function reservations(): HasMany
+    {
+        return $this->hasMany(Reservation::class);
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+    ];
+}

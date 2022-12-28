23a4f94defd4dfedc2159c6d1bb8cf7c1a5ef4a0
Rihitonnnu <hexiliang04@gmail.com>
2022-12-29 02:23:27 +0900

    マイグレーションファイル作成

---
+++ database/migrations/2022_12_28_195841_create_reservations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onUpdate('cascade');
+            $table->foreignId('facility_id')->constrained()->onUpdate('cascade');
+            $table->dateTime('starttime');
+            $table->dateTime('endtime');
+            $table->text('perpose');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+};

5f4e212b9df933830bed70e0d103d78450a02ca3
Rihitonnnu <hexiliang04@gmail.com>
2022-12-29 22:32:38 +0900

    インデント調整

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,6 @@ Route::get('/', function () {
     ]);
 });
 
-
 //未ログインユーザー
 Route::middleware('guest')->group(function () {
     Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
@@ -36,7 +35,7 @@ Route::middleware('guest')->group(function () {
 Route::middleware('auth')->group(function () {
     //ログアウト
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
-    
+
     //ダッシュボード
     Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
@@ -48,31 +47,31 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     //設備
-    Route::get('facility',[FacilityController::class,'index'])->name('facility.index');
+    Route::get('facility', [FacilityController::class, 'index'])->name('facility.index');
 
     //設備の予約関連
-    Route::get('{facilityId}/reservation',[ReservationController::class,'create'])->name('reservation.create');
-    Route::get('{facilityId}/reservation/{reservationId}',[ReservationController::class,'show'])->name('reservation.show');
-    Route::get('{facilityId}/reservation/{reservationId}',[ReservationController::class,'edit'])->name('reservation.edit');
-    Route::post('{facilityId}/reservation',[ReservationController::class,'store'])->name('reservation.store');
-    Route::put('{facilityId}/reservation/{reservationId}',[ReservationController::class,'update'])->name('reservation.update');
-    Route::post('{facilityId}/reservation/{reservationId}',[ReservationController::class,'delete'])->name('reservation.delete');
+    Route::get('{facilityId}/reservation', [ReservationController::class, 'create'])->name('reservation.create');
+    Route::get('{facilityId}/reservation/{reservationId}', [ReservationController::class, 'show'])->name('reservation.show');
+    Route::get('{facilityId}/reservation/{reservationId}', [ReservationController::class, 'edit'])->name('reservation.edit');
+    Route::post('{facilityId}/reservation', [ReservationController::class, 'store'])->name('reservation.store');
+    Route::put('{facilityId}/reservation/{reservationId}', [ReservationController::class, 'update'])->name('reservation.update');
+    Route::post('{facilityId}/reservation/{reservationId}', [ReservationController::class, 'delete'])->name('reservation.delete');
 });
 
 //管理者のみ
 Route::middleware('can:admin')->group(function () {
     //ユーザー関連
-    Route::get('user',[UserController::class,'index'])->name('user.index');
-    Route::get('user/create',[UserController::class,'create'])->name('user.create');
-    Route::get('user/{userId}',[UserController::class,'show'])->name('user.show');
-    Route::post('user/{userId}',[UserController::class,'delete'])->name('user.delete');
+    Route::get('user', [UserController::class, 'index'])->name('user.index');
+    Route::get('user/create', [UserController::class, 'create'])->name('user.create');
+    Route::get('user/{userId}', [UserController::class, 'show'])->name('user.show');
+    Route::post('user/{userId}', [UserController::class, 'delete'])->name('user.delete');
 
     //設備関連
-    Route::get('facility/create',[FacilityController::class,'create'])->name('facility.create');
-    Route::get('facility/{facilityId}',[FacilityController::class,'edit'])->name('facility.edit');
-    Route::post('facility',[FacilityController::class,'store'])->name('facility.store');
-    Route::post('facility/{facilityId}',[FacilityController::class,'update'])->name('facility.update');
-    Route::post('facility/{facilityId}',[FacilityController::class,'delete'])->name('facility.delete');
+    Route::get('facility/create', [FacilityController::class, 'create'])->name('facility.create');
+    Route::get('facility/{facilityId}', [FacilityController::class, 'edit'])->name('facility.edit');
+    Route::post('facility', [FacilityController::class, 'store'])->name('facility.store');
+    Route::post('facility/{facilityId}', [FacilityController::class, 'update'])->name('facility.update');
+    Route::post('facility/{facilityId}', [FacilityController::class, 'delete'])->name('facility.delete');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';

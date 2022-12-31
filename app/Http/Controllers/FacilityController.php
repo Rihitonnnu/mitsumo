26c410ac678687261bc6f114b36b5ad47fae6fa1
Rihitonnnu <hexiliang04@gmail.com>
2022-12-31 21:59:37 +0900

    indexメソッド追加

--- app/Http/Controllers/FacilityController.php
+++ app/Http/Controllers/FacilityController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Facility;
+use Inertia\Inertia;
 
 class FacilityController extends Controller
 {
-    //
+    public function index()
+    {
+        return Inertia::render('Facility/Index', [
+            'facilities'=>Facility::all(),
+            'status' => session('success'),
+        ]);
+    }
 }

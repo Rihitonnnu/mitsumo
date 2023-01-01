631eb287a5435aa0cb36f057d51dd027ed3eb2ff
Rihitonnnu <hexiliang04@gmail.com>
2023-01-01 14:53:01 +0900

    indexメソッド追加

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Facility;
+use App\Models\Reservation;
+use Inertia\Inertia;
 
 class ReservationController extends Controller
 {
-    //
+    /**
+     * 予約一覧画面表示
+     * @param int $id
+     * @return \Inertia\Response
+     */
+    public function index(int $id)
+    {
+        return Inertia::render('Reservation/Index',[
+            'reservations'=>Reservation::where('facility_id',$id)->with(['user'])->get(),
+            'facility_name'=>Facility::find($id)->name,
+        ]);
+    }
 }

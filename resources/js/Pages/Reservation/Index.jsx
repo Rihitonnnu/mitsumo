1de18fa28832f18a038db55ad56ffb03bb0c5ecf
Rihitonnnu <hexiliang04@gmail.com>
2023-01-03 16:06:39 +0900

    検索フォーム作成

--- resources/js/Pages/Reservation/Index.jsx
+++ resources/js/Pages/Reservation/Index.jsx
@@ -1,10 +1,23 @@
 import AnchorButton from "@/Components/AnchorButton";
+import InputLabel from "@/Components/InputLabel";
+import TextInput from "@/Components/TextInput";
 import PrimaryButton from "@/Components/PrimaryButton";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/inertia-react";
+import { useForm } from "@inertiajs/inertia-react";
+import { Input } from "@chakra-ui/react";
 
 export default function Index(props) {
-    console.log(props.reservations);
+    const { data,setData, get } = useForm({
+        subscriber: "",
+        start_time:"",
+    });
+
+    const onSearch = (e) => {
+        e.preventDefault();
+        get(route("reservation.index", props.facility.id));
+    };
+
     return (
         <AuthenticatedLayout auth={props.auth} errors={props.errors}>
             <Head title="予約状況" />
@@ -15,14 +28,65 @@ export default function Index(props) {
                         <div className="p-6 text-gray-900">
                             <div className="flex justify-between md:mb-4">
                                 <h1 className="font-bold text-2xl">
-                                    {props.facility_name}の予約状況
+                                    {props.facility.name}の予約状況
                                 </h1>
                                 <div className="flex">
-                                    <AnchorButton href="" className="">
+                                    <AnchorButton className="text-md">
                                         予約する
                                     </AnchorButton>
                                 </div>
                             </div>
+
+                            <form onSubmit={onSearch}>
+                                <div className="mb-4 border-gray-200 rounded-t-md border-2 box-shadow pb-3">
+                                    <div className="bg-gray-200 rounded-t-md font-bold p-3">
+                                        <p>検索</p>
+                                    </div>
+                                    <div>
+                                        <div className="grid grid-cols-3 p-3">
+                                            <div className="flex p-3 items-center justify-center">
+                                                <InputLabel className="mr-10">
+                                                    予約者名
+                                                </InputLabel>
+                                                <TextInput
+                                                    name="subscriber"
+                                                    id="subscriber"
+                                                    handleChange={(e) =>
+                                                        setData(
+                                                            "subscriber",
+                                                            e.target.value
+                                                        )
+                                                    }
+                                                ></TextInput>
+                                            </div>
+                                            <div className="flex p-3 items-center justify-center">
+                                                <InputLabel className="mr-10">
+                                                    予約日
+                                                </InputLabel>
+                                                <Input
+                                                    size="md"
+                                                    type="datetime-local"
+                                                    id="start_time"
+                                                    name="start_time"
+                                                    onChange={(e) =>
+                                                        setData(
+                                                            "start_time",
+                                                            e.target.value
+                                                        )
+                                                    }
+                                                />
+                                            </div>
+                                        </div>
+
+                                        <div className="text-center">
+                                            <PrimaryButton type="submit">
+                                                検索する
+                                            </PrimaryButton>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+
                             <table className="table-auto w-full text-left whitespace-no-wrap mb-4">
                                 <tbody>
                                     <tr className="border-2 border-gray-300 flex">
